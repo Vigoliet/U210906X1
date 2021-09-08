ca90158a6b6a5106cd7f2375ee9fb9a4a5e5c5ca
Vigor-Lundkvist <vigor.lundkvist@live.se>
2021-09-08 14:29:38 +0200

    Har gjort om arrayen till en 2d array.

--- Uppgift7/Program.cs
+++ Uppgift7/Program.cs
@@ -10,7 +10,7 @@ namespace Uppgift7
             // Översätt till Leeet Speak
             Console.WriteLine("Skriv en mening: "); // ber användaren skriva en mening
             string[] leetArray;
-                                     // A     B      C     D     E     F      G      H     I      J      K     L     M      N      O     P       Q      R      S     T      U      V       W       X        Y     Z
+                                       // A     B      C     D     E     F      G      H     I      J      K     L     M      N      O     P       Q      R      S     T      U      V       W       X        Y     Z
             leetArray = new string[26] { "4" , "|3" , "(" , "|)", "3" , "|=" , "6" , "|-|" , "l", "_|" , "|<" , "1", "|V|" , "/|/", "0" , "|D" , "(,)" , "|2" , "$" , "7" , "|_|" , "|/" , "|/|/" , "><"  , "'/" , "2" };
 
             Console.WriteLine(leetArray[25]);
