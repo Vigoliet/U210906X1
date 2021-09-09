f8e78ec49bedb4358b5361f2c7f97330f51cfba0
vigor-lundkvist <vigor.lundkvist@live.se>
2021-09-09 14:56:40 +0200

    kommenterad kod.

--- Uppgift9/Program.cs
+++ Uppgift9/Program.cs
@@ -8,17 +8,17 @@ namespace Uppgift9
         {
             Console.WriteLine("Mata in hur många tal: ");
 
-            int QuantitiyOfNumbers = Convert.ToInt32(Console.ReadLine());
-            int[] arr = new int[QuantitiyOfNumbers];
+            int QuantitiyOfNumbers = Convert.ToInt32(Console.ReadLine()); //användaren bestämmer antal siffror som skall matas in
+            int[] arr = new int[QuantitiyOfNumbers]; // skapar en array för siffrorna
 
 
-            for (int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < arr.Length; i++) //loop som ber användaren mata in tal tills antal tal = arr (dvs antal tal som användaren först valde)
             {
-                Console.WriteLine("Skriv in ett tal: ");
-                arr[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Skriv in ett tal: "); 
+                arr[i] = Convert.ToInt32(Console.ReadLine()); // gör om inmatad tal från string till integer
 
             }
-
+            // loop tagen från exempel
             int temp;
             for (int j = 0; j <= arr.Length - 2; j++)
             {
