4028b8456bb1df56598a88f53df7e83d1cc8df43
vigor-lundkvist <vigor.lundkvist@live.se>
2021-09-09 14:55:45 +0200

    Har skapat en loop som med hjälp av inmatat värde på hur många och vilka tal som ska sorteras sedan sorteras.

---
+++ Uppgift9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Uppgift9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in hur många tal: ");
+
+            int QuantitiyOfNumbers = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[QuantitiyOfNumbers];
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Skriv in ett tal: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted:");
+            foreach (int p in arr)
+                Console.Write(p + " ");
+            Console.Read();
+        }
+    }
+}
